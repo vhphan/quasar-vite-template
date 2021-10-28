dfc08f6a9dfc8523d527b62f2c1024a0ecf828e6
VH Phan <phanveehuen@gmail.com>
2021-10-28 17:37:19 +0800

    Initial commit

---
+++ src/components/VerTabs.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <q-splitter
+        v-model="splitterModel"
+        style="height: 100vh;"
+    >
+
+      <template v-slot:before>
+        <q-tabs
+            v-model="tab"
+            vertical
+            class="text-teal"
+            :dense="true"
+        >
+          <q-tab name="mails" icon="mail"/>
+          <q-tab name="alarms" icon="alarm"/>
+          <q-tab name="movies" icon="movie"/>
+        </q-tabs>
+      </template>
+
+      <template v-slot:after>
+        <q-tab-panels
+            v-model="tab"
+            animated
+            swipeable
+            vertical
+            transition-prev="jump-up"
+            transition-next="jump-up"
+        >
+          <q-tab-panel name="mails">
+            <div class="text-h4 q-mb-md">Mails</div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure
+              quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam.
+              In, libero.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure
+              quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam.
+              In, libero.</p>
+          </q-tab-panel>
+
+          <q-tab-panel name="alarms">
+            <div class="text-h4 q-mb-md">Alarms</div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure
+              quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam.
+              In, libero.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure
+              quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam.
+              In, libero.</p>
+          </q-tab-panel>
+
+          <q-tab-panel name="movies">
+            <div class="text-h4 q-mb-md">Movies</div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure
+              quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam.
+              In, libero.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure
+              quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam.
+              In, libero.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure
+              quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam.
+              In, libero.</p>
+          </q-tab-panel>
+        </q-tab-panels>
+      </template>
+
+    </q-splitter>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue';
+
+
+export default {
+  name: "VerTabs",
+  setup() {
+    return {
+      tab: ref('mails'),
+      splitterModel: ref(18)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,71 @@
+<template>
+  <q-layout view="lHr lpR lFr">
+
+    <q-header elevated class="bg-primary text-white" height-hint="58">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer"/>
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+
+        <q-tabs align="left">
+          <q-route-tab to="/" label="Home"/>
+          <q-route-tab to="/page2" label="Page Two"/>
+          <q-route-tab to="/page3" label="Page Three"/>
+        </q-tabs>
+        <q-btn dense flat round icon="menu" @click="toggleRightDrawer"/>
+      </q-toolbar>
+
+
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <!-- drawer content -->
+      <VerTabs/>
+    </q-drawer>
+
+    <q-drawer v-model="rightDrawerOpen" side="right" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+import {ref} from 'vue'
+import VerTabs from "../components/VerTabs.vue";
+
+export default {
+  components: {VerTabs},
+  setup() {
+    const leftDrawerOpen = ref(false)
+    const rightDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      },
+
+      rightDrawerOpen,
+      toggleRightDrawer() {
+        rightDrawerOpen.value = !rightDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.q-page-container {
+  padding-top: 0;
+}
+</style>
